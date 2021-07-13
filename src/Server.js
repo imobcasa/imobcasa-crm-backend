e9a533de2b332b05d86d5e5122bcbc1d5b9e4088
Heron Eto <41599309+heroneto@users.noreply.github.com>
2021-07-12 22:52:20 -0300

    fix: fixed origin frontend url

--- src/Server.js
+++ src/Server.js
@@ -22,7 +22,7 @@ class Server {
   _loadMiddlewares(){
     this.app.use(cors({
       credentials: true,
-      origin: ["http://localhost:3000", "https://imobcasa-crm-backend.herokuapp.com"]
+      origin: ["http://localhost:3000", "https://imobcasa-crm.netlify.app"]
     }))
     this.app.use(express.json())
     this.app.use(express.urlencoded({extended: true}))
