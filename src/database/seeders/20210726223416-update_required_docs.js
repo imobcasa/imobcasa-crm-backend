d51661eeefc90c4f18308bf6c42df64d1d209172
Heron Eto <41599309+heroneto@users.noreply.github.com>
2021-07-26 19:38:51 -0300

    feat: update document requireds

---
+++ src/database/seeders/20210726223416-update_required_docs.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+   return queryInterface.bulkUpdate('documentTypes', {
+     required: false
+   }, {
+     key: 'FGTS_DOC'
+   })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+      return queryInterface.bulkUpdate('documentTypes', {
+        required: true
+      }, {
+        key: 'FGTS_DOC'
+      })
+  }
+};
